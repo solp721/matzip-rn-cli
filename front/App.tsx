3af9e781ffee0fe98c660e834455a31f06eb1953
solp721 <easter721@gmail.com>
2025-02-15 14:29:20 +0900

    feat : new

---
+++ front/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+
+export default function App(): React.JSX.Element {
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+
+
+
+
+
+
