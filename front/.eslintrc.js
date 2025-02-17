7db1c19cbb49ca125a10b41e9b67316a1d5b061f
solp721 <easter721@gmail.com>
2025-02-17 23:58:55 +0900

    feat : eslint 설정 추가

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['prettier', '@typescript-eslint', 'react', 'react-native'],
+	env: {
+		node: true,
+		es6: true,
+		'react-native/react-native': true,
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				endOfLine: 'auto',
+			},
+		],
+		'@typescript-eslint/no-require-imports': 'off',
+	},
 };
