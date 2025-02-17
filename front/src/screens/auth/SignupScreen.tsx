389917b01aa155d60063649b1d583580114e2cd6
solp721 <easter721@gmail.com>
2025-02-18 00:03:14 +0900

    feat : 로그인 및 회원가입 스크린

---
+++ front/src/screens/auth/SignupScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function SignupScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>SignupScreen</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
