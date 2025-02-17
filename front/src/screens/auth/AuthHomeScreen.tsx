8f0f933b350b0a5c21c799d1436d2d109e26e071
solp721 <easter721@gmail.com>
2025-02-18 00:04:05 +0900

    feat : 로그인 및 회원가입 스크린

---
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+	View,
+	StyleSheet,
+	SafeAreaView,
+	Image,
+	Dimensions,
+} from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { AuthStackParamList } from '../../navigations/stack/AuthStackNavigator';
+import { authNavigations } from '../../constants/navigations';
+import CustomButton from '../../components/CustomButton';
+
+type AuthHomeScreenProps = StackScreenProps<
+	AuthStackParamList,
+	typeof authNavigations.AUTH_HOME
+>;
+
+export default function AuthHomeScreen({ navigation }: AuthHomeScreenProps) {
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.imageContainer}>
+				<Image
+					resizeMode="contain"
+					source={require('../../assets/matzip.png')}
+					style={styles.image}
+				/>
+			</View>
+			<View style={styles.buttonContainer}>
+				<CustomButton
+					label="로그인하기"
+					onPress={() => navigation.navigate(authNavigations.LOGIN)}
+				/>
+				<CustomButton
+					label="회원가입하기"
+					variant="outlined"
+					onPress={() => navigation.navigate(authNavigations.SIGNUP)}
+				/>
+			</View>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		margin: 30,
+		alignItems: 'center',
+	},
+	imageContainer: {
+		flex: 1.5,
+		width: Dimensions.get('screen').width / 2,
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+	},
+	buttonContainer: {
+		flex: 1,
+		gap: 10,
+	},
+});
