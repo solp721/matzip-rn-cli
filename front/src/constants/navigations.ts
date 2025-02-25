82974be6c54b42790ddcfffe81b0fc981ce96cbd
solp721 <easter721@gmail.com>
2025-02-25 17:39:10 +0900

    feat : 각 네비게이션 네이밍 상수

--- front/src/constants/navigations.ts
+++ front/src/constants/navigations.ts
@@ -1,5 +1,17 @@
-export const authNavigations = {
+const authNavigations = {
 	AUTH_HOME: 'AuthHome',
 	LOGIN: 'Login',
 	SIGNUP: 'Signup',
 } as const;
+
+const mapNavigations = {
+	MAP_HOME: 'MapHome',
+} as const;
+
+const mainDrawerNavigations = {
+	HOME: 'Home',
+	FEED: 'Feed',
+	CALENDAR: 'Calendar',
+} as const;
+
+export { authNavigations, mapNavigations, mainDrawerNavigations };
