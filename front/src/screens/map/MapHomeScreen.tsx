dc98d81a7783cc08cd6de0acf4aa4aee4d7abd83
solp721 <easter721@gmail.com>
2025-02-25 17:47:12 +0900

    feat : 지도 메인화면 스크린

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,16 +1,98 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useRef } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { colors } from '@/constants';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { MapStackParamList } from '@/navigations/stack/MapStackNavigator';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { CompositeNavigationProp } from '@react-navigation/native';
+import { MainDrawerParamList } from '@/navigations/drawer/MainDrawerNavigator';
+import useUserLocation from '@/hooks/useUserLocation';
+import usePermission from '@/hooks/usePermission';
+type Navigation = CompositeNavigationProp<
+	StackNavigationProp<MapStackParamList>,
+	DrawerNavigationProp<MainDrawerParamList>
+>;
 
 export default function MapHomeScreen() {
+	const inset = useSafeAreaInsets();
+	const navigation = useNavigation<Navigation>();
+	const { userLocation, isUserLocationError } = useUserLocation();
+	const mapRef = useRef<MapView | null>(null);
+	usePermission('LOCATION');
+
+	const handlePressUserLocation = () => {
+		if (isUserLocationError) {
+			return;
+		}
+		mapRef.current?.animateToRegion({
+			latitude: userLocation.latitude,
+			longitude: userLocation.longitude,
+			latitudeDelta: 0.0922,
+			longitudeDelta: 0.0421,
+		});
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>MapHomeScreen</Text>
-		</View>
+		<>
+			<MapView
+				ref={mapRef}
+				style={styles.container}
+				provider={PROVIDER_GOOGLE}
+				showsUserLocation
+				followsUserLocation
+				showsMyLocationButton={false}
+			/>
+			<Pressable
+				style={[styles.drawerButton, { top: inset.top || 20 }]}
+				onPress={() => navigation.openDrawer()}
+			>
+				<Text>서랍</Text>
+			</Pressable>
+			<View style={styles.buttonList}>
+				<Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+					<Text>내위치</Text>
+				</Pressable>
+			</View>
+		</>
 	);
 }
-
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+	},
+	drawerButton: {
+		position: 'absolute',
+		left: 0,
+		top: 20,
+		paddingVertical: 10,
+		paddingHorizontal: 12,
+		backgroundColor: colors.PINK_700,
+		borderTopRightRadius: 50,
+		borderBottomRightRadius: 50,
+		shadowColor: colors.BLACK,
+		shadowOffset: { width: 1, height: 1 },
+		shadowOpacity: 0.5,
+		elevation: 4,
+	},
+	buttonList: {
+		position: 'absolute',
+		bottom: 30,
+		right: 15,
+	},
+	mapButton: {
+		backgroundColor: colors.PINK_700,
+		marginVertical: 5,
+		height: 48,
+		width: 48,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 30,
+		shadowColor: colors.BLACK,
+		shadowOffset: { width: 1, height: 2 },
+		shadowOpacity: 0.5,
+		elevation: 2,
 	},
 });
