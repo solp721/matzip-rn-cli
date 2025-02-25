c56eca208afd57dfa02e2c016baccb67ddbb927e
solp721 <easter721@gmail.com>
2025-02-25 17:42:50 +0900

    fix : 네이밍 상수화 변경

--- front/src/navigations/drawer/MainDrawerNavigator.tsx
+++ front/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -1,16 +1,48 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import MapHomeScreen from '../../screens/map/MapHomeScreen';
-import FeedHomeScreen from '../../screens/map/FeedHomeScreen';
-import CalendarHomeScreen from '../../screens/calendar/CalendarHomeScreen';
+import FeedHomeScreen from '@/screens/map/FeedHomeScreen';
+import CalendarHomeScreen from '@/screens/calendar/CalendarHomeScreen';
+import MapStackNavigator from '@/navigations/stack/MapStackNavigator';
+import { mainDrawerNavigations } from '@/constants';
+import { MapStackParamList } from '@/navigations/stack/MapStackNavigator';
+import { NavigatorScreenParams } from '@react-navigation/native';
 
-const Drawer = createDrawerNavigator();
+export type MainDrawerParamList = {
+	[mainDrawerNavigations.HOME]: NavigatorScreenParams<MapStackParamList>;
+	[mainDrawerNavigations.FEED]: undefined;
+	[mainDrawerNavigations.CALENDAR]: undefined;
+};
+
+const Drawer = createDrawerNavigator<MainDrawerParamList>();
 
 export default function MainDrawerNavigator() {
 	return (
-		<Drawer.Navigator>
-			<Drawer.Screen name="MapHome" component={MapHomeScreen} />
-			<Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
-			<Drawer.Screen name="CalendarHome" component={CalendarHomeScreen} />
+		<Drawer.Navigator
+			screenOptions={{
+				headerShown: false,
+				drawerType: 'front',
+			}}
+		>
+			<Drawer.Screen
+				name={mainDrawerNavigations.HOME}
+				component={MapStackNavigator}
+				options={{
+					title: '홈',
+				}}
+			/>
+			<Drawer.Screen
+				name={mainDrawerNavigations.FEED}
+				component={FeedHomeScreen}
+				options={{
+					title: '피드',
+				}}
+			/>
+			<Drawer.Screen
+				name={mainDrawerNavigations.CALENDAR}
+				component={CalendarHomeScreen}
+				options={{
+					title: '캘린더',
+				}}
+			/>
 		</Drawer.Navigator>
 	);
 }
