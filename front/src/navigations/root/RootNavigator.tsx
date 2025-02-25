e15d97f8bd8aa067e9857dcb784a2c328cd88e1d
solp721 <easter721@gmail.com>
2025-02-25 17:43:18 +0900

    fix : 인증상태 네비게이션 변경

--- front/src/navigations/root/RootNavigator.tsx
+++ front/src/navigations/root/RootNavigator.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import AuthStackNavigator from '../stack/AuthStackNavigator';
-import MainDrawerNavigator from '../drawer/MainDrawerNavigator';
+import AuthStackNavigator from '@/navigations/stack/AuthStackNavigator';
+import MainDrawerNavigator from '@/navigations/drawer/MainDrawerNavigator';
+import { useAuth } from '@/hooks/queries/useAuth';
 
 export default function RootNavigator() {
-	const isLoggedIn = false;
+	const { isLogin } = useAuth();
 
-	return <>{isLoggedIn ? <MainDrawerNavigator /> : <AuthStackNavigator />}</>;
+	return <>{isLogin ? <MainDrawerNavigator /> : <AuthStackNavigator />}</>;
 }
