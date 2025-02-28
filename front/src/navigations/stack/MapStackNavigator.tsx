8cf0a46f48c396ac0a8d2e753e460606ec640495
solp721 <easter721@gmail.com>
2025-02-28 14:27:49 +0900

    feat : 장소추가 버튼 추가

--- front/src/navigations/stack/MapStackNavigator.tsx
+++ front/src/navigations/stack/MapStackNavigator.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MapHomeScreen from '@/screens/map/MapHomeScreen';
 import { mapNavigations } from '@/constants';
+import AddPostScreen from '@/screens/map/AddPostScreen';
+import { LatLng } from 'react-native-maps';
 
 export type MapStackParamList = {
 	[mapNavigations.MAP_HOME]: undefined;
+	[mapNavigations.ADD_POST]: {
+		location: LatLng;
+	};
 };
 
 const Stack = createStackNavigator<MapStackParamList>();
@@ -32,6 +37,13 @@ export default function MapStackNavigator() {
 				options={{
 					headerTitle: '',
 					headerShown: false,
+				}}
+			/>
+			<Stack.Screen
+				name={mapNavigations.ADD_POST}
+				component={AddPostScreen}
+				options={{
+					headerTitle: '장소 추가',
 				}}
 			/>
 		</Stack.Navigator>
