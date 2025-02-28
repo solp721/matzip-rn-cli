c163cb34c46f6d606e63bea40aa33e59be6472ff
solp721 <easter721@gmail.com>
2025-02-28 14:25:07 +0900

    feat : 컬러 상수 추가

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,11 +1,18 @@
 export const colors = {
 	WHITE: '#fff',
 	BLACK: '#000',
+	GRAY_100: '#F8F8F8',
 	GRAY_200: '#E7E7E7',
 	GRAY_500: '#828282',
 	GRAY_700: '#575757',
+	PINK_200: '#FAE2E9',
+	PINK_400: '#EC87A5',
 	PINK_500: '#BF5C79',
 	PINK_700: '#C63B64',
 	RED_300: '#FFB4B4',
 	RED_500: '#FF5F5F',
+	BLUE_400: '#B4E0FF',
+	GREEN_400: '#CCE6BA',
+	YELLOW_400: '#FFE594',
+	PURPLE_400: '#C4C4E7',
 } as const;
