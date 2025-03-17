863d483a43d375c5b5b5030c2bc7780cc4d38608
solp721 <easter721@gmail.com>
2025-03-17 21:57:09 +0900

    feat : 이미지 api

---
+++ front/src/api/image.ts
@@ -0,0 +1,12 @@
+import axiosInstance from './axios';
+
+const uploadImages = async (body: FormData): Promise<string[]> => {
+	const { data } = await axiosInstance.post('/images', body, {
+		headers: {
+			'Content-Type': 'multipart/form-data',
+		},
+	});
+	return data;
+};
+
+export { uploadImages };
