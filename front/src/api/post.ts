df8cf0df83c10a436f42110aae6d725f0c34fd06
solp721 <easter721@gmail.com>
2025-03-19 01:04:59 +0900

    feat : 즐겨찾기 api

--- front/src/api/post.ts
+++ front/src/api/post.ts
@@ -47,7 +47,27 @@ const updatePost = async ({
 	return data;
 };
 
-export { createPost, getPost, getPosts, deletePost, updatePost };
+const getFavoritePost = async (page = 1): Promise<ResponsePost[]> => {
+	const { data } = await axiosInstance.get(`/favorite/my?page=${page}`);
+
+	return data;
+};
+
+const updateFavoritePost = async (id: number): Promise<number> => {
+	const { data } = await axiosInstance.post(`/favorites/${id}`);
+
+	return data;
+};
+
+export {
+	createPost,
+	getPost,
+	getPosts,
+	deletePost,
+	updatePost,
+	updateFavoritePost,
+	getFavoritePost,
+};
 export type {
 	ResponsePost,
 	RequestCreatePost,
