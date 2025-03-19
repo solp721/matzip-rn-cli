8df75f6be716f711e57a9f8d63d3c47942ec98fc
solp721 <easter721@gmail.com>
2025-03-19 20:23:38 +0900

    feat : 캘린더 스크린

--- front/src/screens/calendar/CalendarHomeScreen.tsx
+++ front/src/screens/calendar/CalendarHomeScreen.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { colors } from '@/constants';
+import Calendar from '@/components/calendar/Calendar';
+import { getMonthYearDetails, getNewMonthYear } from '@/utils/date';
 
 export default function CalendarHomeScreen() {
+	const currentMonthYear = getMonthYearDetails(new Date());
+	const [monthYear, setMonthYear] = useState(currentMonthYear);
+
+	const handleUpdateMonth = (increment: number) => {
+		setMonthYear(prev => getNewMonthYear(prev, increment));
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>CalendarHomeScreen</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<Calendar monthYear={monthYear} onChangeMonth={handleUpdateMonth} />
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: colors.WHITE,
 	},
 });
