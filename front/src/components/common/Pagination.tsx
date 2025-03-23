7e3c85145f1c0af2a6c3923fb10afea379f7ae85
solp721 <easter721@gmail.com>
2025-03-23 23:00:04 +0900

    feat : 장소 검색 페이지네이션

---
+++ front/src/components/common/Pagination.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import Octicons from 'react-native-vector-icons/Octicons';
+import { IconProps } from 'react-native-vector-icons/Icon';
+
+const OctIcon = Octicons as unknown as React.ComponentType<IconProps>;
+
+import { colors } from '@/constants';
+
+interface PaginationProps {
+	pageParam: number;
+	fetchPrevPage: () => void;
+	fetchNextPage: () => void;
+	hasNextPage: boolean;
+	totalLength: number;
+}
+
+export default function Pagination({
+	pageParam,
+	fetchPrevPage,
+	fetchNextPage,
+	hasNextPage,
+	totalLength,
+}: PaginationProps) {
+	return (
+		<View style={styles.container}>
+			<Pressable
+				onPress={fetchPrevPage}
+				style={styles.pageButton}
+				disabled={pageParam <= 1}
+			>
+				<OctIcon
+					name="arrow-left"
+					size={15}
+					color={pageParam > 1 ? colors.BLACK : colors.GRAY_300}
+					onPress={fetchPrevPage}
+					disabled={pageParam <= 1}
+				/>
+				<Text style={pageParam > 1 ? styles.pageText : styles.disabledPageText}>
+					이전페이지
+				</Text>
+			</Pressable>
+
+			<Pressable
+				onPress={fetchNextPage}
+				style={styles.pageButton}
+				disabled={totalLength === 0 || !hasNextPage}
+			>
+				<Text
+					style={
+						totalLength > 0 && hasNextPage
+							? styles.pageText
+							: styles.disabledPageText
+					}
+				>
+					다음페이지
+				</Text>
+				<OctIcon
+					name="arrow-right"
+					size={15}
+					color={
+						totalLength > 0 && hasNextPage ? colors.BLACK : colors.GRAY_300
+					}
+					onPress={fetchNextPage}
+					disabled={totalLength === 0 || !hasNextPage}
+				/>
+			</Pressable>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: 10,
+		marginHorizontal: 5,
+	},
+	pageButton: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 5,
+		height: 25,
+	},
+	pageText: {
+		fontSize: 15,
+		color: colors.BLACK,
+	},
+	disabledPageText: {
+		fontSize: 15,
+		color: colors.GRAY_300,
+	},
+});
