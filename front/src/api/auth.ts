cf6a03b2a150263385c40e9ce41dba77f606cbcd
solp721 <easter721@gmail.com>
2025-03-24 00:42:02 +0900

    feat : 카카오 로그인 api

--- front/src/api/auth.ts
+++ front/src/api/auth.ts
@@ -33,6 +33,14 @@ const postLogin = async ({
 	return data;
 };
 
+const kakaoLogin = async (token: string): Promise<ResponseToken> => {
+	const { data } = await axiosInstance.post('/auth/oauth/kakao', {
+		token,
+	});
+
+	return data;
+};
+
 type ResponseProfile = Profile & Category;
 
 const getProfile = async (): Promise<ResponseProfile> => {
@@ -57,5 +65,12 @@ const logout = async (): Promise<void> => {
 	await axiosInstance.post('/auth/logout');
 };
 
-export { postSignup, postLogin, getProfile, getAccessToken, logout };
+export {
+	postSignup,
+	postLogin,
+	getProfile,
+	getAccessToken,
+	logout,
+	kakaoLogin,
+};
 export type { RequestUser, ResponseToken, ResponseProfile };
