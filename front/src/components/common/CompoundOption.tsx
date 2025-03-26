53e2d8ef1faf7d235a42b546e9ed259804a66c90
solp721 <easter721@gmail.com>
2025-03-27 01:02:17 +0900

    feat : 필터 및 체크박스 합성컴포넌트

--- front/src/components/common/CompoundOption.tsx
+++ front/src/components/common/CompoundOption.tsx
@@ -12,12 +12,13 @@ import {
 } from 'react-native';
 import { colors } from '@/constants';
 import IonIcons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { IconProps } from 'react-native-vector-icons/Icon';
 import useThemeStorage from '@/hooks/useThemeStorage';
 import { ThemeMode } from '@/types';
 
 const IonIcon = IonIcons as unknown as React.ComponentType<IconProps>;
-
+const MaterialIcon = MaterialIcons as unknown as React.ComponentType<IconProps>;
 interface OptionContextValue {
 	onClickOutSide?: (event: GestureResponderEvent) => void;
 }
@@ -132,12 +133,71 @@ function Divider() {
 	return <View style={styles.border} />;
 }
 
+interface CheckBoxProps extends PressableProps {
+	children: ReactNode;
+	icon?: ReactNode;
+	isChecked: boolean;
+}
+
+function CheckBox({
+	children,
+	icon,
+	isChecked = false,
+	...props
+}: CheckBoxProps) {
+	const { theme } = useThemeStorage();
+	const styles = styling(theme);
+
+	return (
+		<Pressable
+			{...props}
+			style={({ pressed }) => [
+				pressed && styles.optionButtonPressed,
+				styles.checkBoxContainer,
+			]}
+		>
+			<IonIcon
+				name={`checkmark-circle${isChecked ? '' : '-outline'}`}
+				size={22}
+				color={colors[theme].BLUE_500}
+			/>
+			{icon}
+			<Text style={styles.checkBoxText}>{children}</Text>
+		</Pressable>
+	);
+}
+
+interface FilterProps extends PressableProps {
+	children: ReactNode;
+	isSelected?: boolean;
+}
+
+function Filter({ children, isSelected, ...props }: FilterProps) {
+	const { theme } = useThemeStorage();
+	const styles = styling(theme);
+
+	return (
+		<Pressable style={styles.filterContainer} {...props}>
+			<Text style={isSelected ? styles.filterSelectedText : styles.filterText}>
+				{children}
+			</Text>
+			<MaterialIcon
+				name="keyboard-arrow-down"
+				size={22}
+				color={isSelected ? colors[theme].BLUE_500 : colors[theme].GRAY_500}
+			/>
+		</Pressable>
+	);
+}
+
 export const CompoundOption = Object.assign(OptionMain, {
 	Container,
 	Background,
 	Button,
 	Title,
 	Divider,
+	CheckBox,
+	Filter,
 });
 
 const styling = (theme: ThemeMode) =>
@@ -184,5 +244,32 @@ const styling = (theme: ThemeMode) =>
 		border: {
 			borderBottomColor: colors[theme].GRAY_200,
 			borderBottomWidth: 1,
+		},
+		checkBoxContainer: {
+			flexDirection: 'row',
+			alignItems: 'center',
+			paddingVertical: 10,
+			paddingHorizontal: 30,
+			gap: 10,
+		},
+		checkBoxText: {
+			fontSize: 15,
+			color: colors[theme].BLACK,
+		},
+		filterContainer: {
+			flexDirection: 'row',
+			alignItems: 'center',
+			padding: 10,
+			gap: 5,
+		},
+		filterText: {
+			fontSize: 15,
+			fontWeight: '500',
+			color: colors[theme].GRAY_300,
+		},
+		filterSelectedText: {
+			fontSize: 15,
+			fontWeight: '500',
+			color: colors[theme].BLUE_500,
 		},
 	});
