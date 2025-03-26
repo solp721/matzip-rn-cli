e1a9a700fd2e0eec63517aafe75a616e69f6bf7b
solp721 <easter721@gmail.com>
2025-03-26 14:09:28 +0900

    feat : 계정 삭제 및 카테고리 수정 네비게이션 리스트 추가 및 스크린 추가

--- front/src/navigations/stack/SettingStackNavigator.tsx
+++ front/src/navigations/stack/SettingStackNavigator.tsx
@@ -8,10 +8,14 @@ import { MainDrawerParamList } from '@/navigations/drawer/MainDrawerNavigator';
 import EditProfileScreen from '@/screens/setting/EditProfileScreen';
 import SettingHomeScreen from '@/screens/setting/SettingHomeScreen';
 import SettingHeaderLeft from '@/components/setting/SettingHeaderLeft';
+import DeleteAccountScreen from '@/screens/setting/DeleteAccountScreen';
+import EditCategoryScreen from '@/screens/setting/EditCategoryScreen';
 
 export type SettingStackParamList = {
 	[settingNavigations.SETTING_HOME]: undefined;
 	[settingNavigations.EDIT_PROFILE]: undefined;
+	[settingNavigations.DELETE_ACCOUNT]: undefined;
+	[settingNavigations.EDIT_CATEGORY]: undefined;
 };
 
 export type SettingStackNavigationProp = CompositeNavigationProp<
@@ -55,6 +59,26 @@ export default function SettingStackNavigator() {
 				component={EditProfileScreen}
 				options={() => ({
 					headerTitle: '프로필 수정',
+					cardStyle: {
+						backgroundColor: colors.WHITE,
+					},
+				})}
+			/>
+			<Stack.Screen
+				name={settingNavigations.DELETE_ACCOUNT}
+				component={DeleteAccountScreen}
+				options={() => ({
+					headerTitle: '회원탈퇴',
+					cardStyle: {
+						backgroundColor: colors.WHITE,
+					},
+				})}
+			/>
+			<Stack.Screen
+				name={settingNavigations.EDIT_CATEGORY}
+				component={EditCategoryScreen}
+				options={() => ({
+					headerTitle: '카테고리 설정',
 					cardStyle: {
 						backgroundColor: colors.WHITE,
 					},
