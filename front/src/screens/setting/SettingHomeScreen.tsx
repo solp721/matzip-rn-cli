77f5418d6a0841e292f063b76d251c7cc97792e5
solp721 <easter721@gmail.com>
2025-03-26 14:25:08 +0900

    feat : 테마 모달 및 카테고리 설정 prop

--- front/src/screens/setting/SettingHomeScreen.tsx
+++ front/src/screens/setting/SettingHomeScreen.tsx
@@ -1,12 +1,14 @@
 import SettingItem from '@/components/setting/SettingItem';
 import { colors, settingNavigations } from '@/constants';
 import { useAuth } from '@/hooks/queries/useAuth';
+import useModal from '@/hooks/useModal';
 import { SettingStackParamList } from '@/navigations/stack/SettingStackNavigator';
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
 import { StyleSheet, SafeAreaView, ScrollView, View } from 'react-native';
 import { IconProps } from 'react-native-vector-icons/Icon';
 import OctIcons from 'react-native-vector-icons/Octicons';
+import DarkModeOption from '@/components/setting/DarkModeOption';
 
 const OctIcon = OctIcons as unknown as React.ComponentType<IconProps>;
 
@@ -16,6 +18,7 @@ export default function SettingHomeScreen({
 	navigation,
 }: SettingHomeScreenProps) {
 	const { logoutMutation } = useAuth();
+	const darkModeOption = useModal();
 
 	const handlePressEditProfile = () => {
 		navigation.navigate(settingNavigations.EDIT_PROFILE);
@@ -26,12 +29,20 @@ export default function SettingHomeScreen({
 		console.log(logoutMutation);
 	};
 
+	const handlePressEditCategory = () => {
+		navigation.navigate(settingNavigations.EDIT_CATEGORY);
+	};
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<ScrollView>
 				<View style={styles.space} />
 				<SettingItem title="프로필 수정" onPress={handlePressEditProfile} />
-				<SettingItem title="마커 카테고리 설정" />
+				<SettingItem
+					title="마커 카테고리 설정"
+					onPress={handlePressEditCategory}
+				/>
+				<SettingItem title="테마 설정" onPress={darkModeOption.show} />
 				<View style={styles.space} />
 				<SettingItem
 					title="로그아웃"
@@ -40,6 +51,10 @@ export default function SettingHomeScreen({
 					onPress={handlePressLogout}
 				/>
 			</ScrollView>
+			<DarkModeOption
+				isVisible={darkModeOption.isVisible}
+				hideOption={darkModeOption.hide}
+			/>
 		</SafeAreaView>
 	);
 }
