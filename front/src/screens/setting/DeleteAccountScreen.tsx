d943b75905d499f8815d179ef6401dd8e8dbed80
solp721 <easter721@gmail.com>
2025-03-26 14:20:11 +0900

    feat : 계정 삭제 스크린

---
+++ front/src/screens/setting/DeleteAccountScreen.tsx
@@ -0,0 +1,87 @@
+import CustomButton from '@/components/common/CustomButton';
+import { colors, errorMessages } from '@/constants';
+import { useAuth } from '@/hooks/queries/useAuth';
+import React from 'react';
+import { StyleSheet, View, Text, Alert } from 'react-native';
+import { alerts } from '@/constants';
+import Toast from 'react-native-toast-message';
+export default function DeleteAccountScreen() {
+	const { deleteAccountMutation } = useAuth();
+
+	const handlePressDeleteAccount = () => {
+		Alert.alert(
+			alerts.DELETE_ACCOUNT.TITLE,
+			alerts.DELETE_ACCOUNT.DESCRIPTION,
+			[
+				{
+					text: '탈퇴',
+					onPress: () =>
+						deleteAccountMutation.mutate(null, {
+							onSuccess: () => {
+								Toast.show({
+									type: 'success',
+									text1: '회원탈퇴가 완료되었습니다.',
+									position: 'bottom',
+								});
+							},
+							onError: error => {
+								Toast.show({
+									type: 'error',
+									text1:
+										error.response?.data.message ||
+										errorMessages.UNEXPECTED_ERROR,
+									position: 'bottom',
+								});
+							},
+						}),
+					style: 'destructive',
+				},
+				{
+					text: '취소',
+					style: 'cancel',
+				},
+			],
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.infoContainer}>
+				<Text style={styles.infoText}>
+					저장된 데이터를 모두 삭제해야 회원탈퇴가 가능해요.
+				</Text>
+				<Text style={styles.infoText}>
+					저장된 장소가 남아있다면 삭제해주세요.
+				</Text>
+				<Text style={styles.infoText}>
+					회원탈퇴 후 데이터는 복구할 수 없어요.
+				</Text>
+			</View>
+
+			<CustomButton label="회원탈퇴" onPress={handlePressDeleteAccount} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		marginBottom: 20,
+	},
+	infoContainer: {
+		alignItems: 'center',
+		marginTop: 10,
+		marginBottom: 30,
+		borderWidth: 1,
+		borderColor: colors.PINK_700,
+		borderRadius: 3,
+		padding: 10,
+		gap: 10,
+	},
+	infoText: {
+		color: colors.PINK_700,
+		fontSize: 15,
+		fontWeight: '600',
+	},
+});
