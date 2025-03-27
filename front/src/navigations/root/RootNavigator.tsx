719200876401dbd1d0a52f1becb70377a1e1b9d4
solp721 <easter721@gmail.com>
2025-03-27 16:34:05 +0900

    feat : 루트 네비게이션 에러바운더리 적용

--- front/src/navigations/root/RootNavigator.tsx
+++ front/src/navigations/root/RootNavigator.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import AuthStackNavigator from '@/navigations/stack/AuthStackNavigator';
 import MainDrawerNavigator from '@/navigations/drawer/MainDrawerNavigator';
 import { useAuth } from '@/hooks/queries/useAuth';
-
+import RetryErrorBoundary from '@/components/common/RetryErrorBoundary';
 export default function RootNavigator() {
 	const { isLogin } = useAuth();
 
-	return <>{isLogin ? <MainDrawerNavigator /> : <AuthStackNavigator />}</>;
+	return (
+		<>
+			<RetryErrorBoundary>
+				{isLogin ? <MainDrawerNavigator /> : <AuthStackNavigator />}
+			</RetryErrorBoundary>
+		</>
+	);
 }
