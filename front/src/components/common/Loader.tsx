e0811dc12ac751e9786d3eda99453401fa73f22a
solp721 <easter721@gmail.com>
2025-03-27 15:18:22 +0900

    faet : 로딩 컴포넌트

---
+++ front/src/components/common/Loader.tsx
@@ -0,0 +1,39 @@
+import React, { PropsWithChildren } from 'react';
+import {
+	ActivityIndicator,
+	ActivityIndicatorProps,
+	StyleSheet,
+	View,
+} from 'react-native';
+
+import { colors } from '@/constants';
+
+export default function Loader({
+	children,
+	size = 'small',
+	color = colors.light.GRAY_500,
+	...props
+}: PropsWithChildren<ActivityIndicatorProps>) {
+	return (
+		<View style={styles.container}>
+			<ActivityIndicator
+				size={size}
+				color={color}
+				style={styles.indicator}
+				{...props}
+			/>
+			{children}
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	indicator: {
+		marginBottom: 20,
+	},
+});
