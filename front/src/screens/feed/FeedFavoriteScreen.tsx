fc2fb63a8feaf2d097fdf0d0918983c3318926d6
solp721 <easter721@gmail.com>
2025-03-27 15:19:11 +0900

    feat : 서스펜스 추가

--- front/src/screens/feed/FeedFavoriteScreen.tsx
+++ front/src/screens/feed/FeedFavoriteScreen.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { colors } from '@/constants';
 import FeedFavoriteList from '@/components/feed/FeedFavoriteList';
 import useThemeStorage from '@/hooks/useThemeStorage';
 import { ThemeMode } from '@/types';
+import Loader from '@/components/common/Loader';
 
 export default function FeedFavoriteScreen() {
 	const { theme } = useThemeStorage();
@@ -11,7 +12,9 @@ export default function FeedFavoriteScreen() {
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<FeedFavoriteList />
+			<Suspense fallback={<Loader />}>
+				<FeedFavoriteList />
+			</Suspense>
 		</SafeAreaView>
 	);
 }
--- front/src/screens/feed/FeedHomeScreen.tsx
+++ front/src/screens/feed/FeedHomeScreen.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import FeedList from '@/components/feed/FeedList';
-
+import Loader from '@/components/common/Loader';
 export default function FeedHomeScreen() {
 	return (
 		<SafeAreaView style={styles.container}>
-			<FeedList />
+			<Suspense fallback={<Loader />}>
+				<FeedList />
+			</Suspense>
 		</SafeAreaView>
 	);
 }
