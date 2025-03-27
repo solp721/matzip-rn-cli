776318e978b74cdc0f67837e735cf64707f2587a
solp721 <easter721@gmail.com>
2025-03-27 16:33:27 +0900

    feat : 서버 에러일때만 에러바운더리 표시

--- front/src/hooks/queries/useAuth.ts
+++ front/src/hooks/queries/useAuth.ts
@@ -31,6 +31,7 @@ import { Category, Profile } from '@/types/domain';
 function useSignup(mutationOptions?: UseMutationCustomOptions) {
 	return useMutation({
 		mutationFn: postSignup,
+		throwOnError: error => Number(error.response?.status) >= 500,
 		...mutationOptions,
 	});
 }
@@ -53,6 +54,7 @@ function useLogin<T>(
 				queryKey: [queryKeys.AUTH, queryKeys.GET_PROFILE],
 			});
 		},
+		throwOnError: error => Number(error.response?.status) >= 500,
 		...mutationOptions,
 	});
 }
